073cc0239e4a05f0efc2a5ea6a062dee24366e5a
YordanovDY <yordanovdy23@gmail.com>
2025-05-08 18:19:41 +0300

    Create data models

---
+++ prisma/migrations/20250508130027_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "role_name" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "role_id" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/services/auth-service.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+import { PrismaClient } from "../generated/prisma";
+import { RegisterBody } from "../dtos/auth.dto";
+import { User } from "../types/auth-types";
+
+const prisma = new PrismaClient().user;
+
+async function register(data: RegisterBody): Promise<User> {
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+    const newUser = await prisma.create({ data: { ...data, password: hashedPassword } });
+
+    return { ...newUser };
+}
+
+const authService = {
+    register
+}
+
+export default authService;
